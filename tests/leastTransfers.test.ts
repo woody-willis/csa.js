6be4984615e2b42b5e6919474329bb332c168f5a
Woody Willis <woodyowillis@gmail.com>
2025-04-17 11:08:02 +0000

    fix: least transfer testing

--- tests/leastTransfers.test.ts
+++ tests/leastTransfers.test.ts
@@ -30,7 +30,7 @@ describe('Least Transfers', () => {
         expect(csa.instance).toBeInstanceOf(CSALeastTransfers);
     });
 
-    test('should produce the correct least transfers time with no changes', async () => {
+    test('should produce the correct least transfers with no changes', async () => {
         const csa = createCSAInstance();
 
         const sourceStop: Stop = { id: 'BOH', name: 'Bosham' };
@@ -60,43 +60,63 @@ describe('Least Transfers', () => {
         );
     });
 
-    // test('should produce the correct least transfers time with 1 change', async () => {
-    //     const csa = createCSAInstance();
+    test('should produce the correct least transfers with 1 change', async () => {
+        const csa = createCSAInstance();
 
-    //     const sourceStop: Stop = { id: 'GLD', name: 'Guildford' };
-    //     const targetStop: Stop = { id: 'BSK', name: 'Basingstoke' };
+        const sourceStop: Stop = { id: 'GLD', name: 'Guildford' };
+        const targetStop: Stop = { id: 'BSK', name: 'Basingstoke' };
 
-    //     const journey = await csa.run(sourceStop, new Date('2025-03-26T17:01:00Z'), targetStop);
-    //     const uniqueServices = new Set(journey.map((connection) => (connection.service as Service).id));
+        const journey = await csa.run(
+            sourceStop,
+            new Date('2025-03-26T17:01:00Z'),
+            targetStop
+        );
+        const uniqueServices = new Set(
+            journey.map((connection) => (connection.service as Service).id)
+        );
 
-    //     expect(journey).toBeDefined();
-    //     expect(journey.length).toBe(8);
-    //     expect(uniqueServices.size).toBe(2);
+        expect(journey).toBeDefined();
+        expect(journey.length).toBe(8);
+        expect(uniqueServices.size).toBe(2);
 
-    //     expect((journey[0].departureStop as Stop).id).toEqual('GLD');
-    //     expect((journey[7].arrivalStop as Stop).id).toEqual('BSK');
+        expect((journey[0].departureStop as Stop).id).toEqual('GLD');
+        expect((journey[7].arrivalStop as Stop).id).toEqual('BSK');
 
-    //     expect(journey[0].departureTime.toISOString()).toEqual('2025-03-26T17:02:00.000Z');
-    //     expect(journey[7].arrivalTime.toISOString()).toEqual('2025-03-26T17:58:01.000Z');
-    // });
+        expect(journey[0].departureTime.toISOString()).toEqual(
+            '2025-03-26T17:02:00.000Z'
+        );
+        expect(journey[7].arrivalTime.toISOString()).toEqual(
+            '2025-03-26T17:58:01.000Z'
+        );
+    });
 
-    // test('should produce the correct least transfers time with 2 changes', async () => {
-    //     const csa = createCSAInstance();
+    test('should produce the correct least transfers with 1 change despite a faster 2 change route', async () => {
+        const csa = createCSAInstance();
 
-    //     const sourceStop: Stop = { id: 'AHV', name: 'Ash Vale' };
-    //     const targetStop: Stop = { id: 'WIN', name: 'Winchester' };
+        const sourceStop: Stop = { id: 'AHV', name: 'Ash Vale' };
+        const targetStop: Stop = { id: 'WIN', name: 'Winchester' };
 
-    //     const journey = await csa.run(sourceStop, new Date('2025-03-26T18:09:00Z'), targetStop);
-    //     const uniqueServices = new Set(journey.map((connection) => (connection.service as Service).id));
+        const journey = await csa.run(
+            sourceStop,
+            new Date('2025-03-26T18:09:00Z'),
+            targetStop
+        );
+        const uniqueServices = new Set(
+            journey.map((connection) => (connection.service as Service).id)
+        );
 
-    //     expect(journey).toBeDefined();
-    //     expect(journey.length).toBe(8);
-    //     expect(uniqueServices.size).toBe(3);
+        expect(journey).toBeDefined();
+        expect(journey.length).toBe(3);
+        expect(uniqueServices.size).toBe(2);
 
-    //     expect((journey[0].departureStop as Stop).id).toEqual('AHV');
-    //     expect((journey[7].arrivalStop as Stop).id).toEqual('WIN');
+        expect((journey[0].departureStop as Stop).id).toEqual('AHV');
+        expect((journey[2].arrivalStop as Stop).id).toEqual('WIN');
 
-    //     expect(journey[0].departureTime.toISOString()).toEqual('2025-03-26T18:09:01.000Z');
-    //     expect(journey[7].arrivalTime.toISOString()).toEqual('2025-03-26T19:21:30.000Z');
-    // });
+        expect(journey[0].departureTime.toISOString()).toEqual(
+            '2025-03-26T18:09:01.000Z'
+        );
+        expect(journey[2].arrivalTime.toISOString()).toEqual(
+            '2025-03-26T20:31:30.000Z'
+        );
+    });
 });
